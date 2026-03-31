97a5fccdaeb58d0cd5606155b0f8831a543277f8
l0rrane <gabrielalorraneespin@gmail.com>
2026-03-31 15:06:54 -0300

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/site-zyra/", // 🔥 ESSA LINHA RESOLVE TUDO
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
